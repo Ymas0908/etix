6e2e267669fd1cffb54eb83a4d1e8ede954d4a3f
KEBE <samuel.kebe@it-centrex.com>
2024-12-20 11:25:48 +0000

    First commit

---
+++ src/test/java/com/tix/etix/EtixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tix.etix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
