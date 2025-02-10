1b6303ef0b802246dfd8935b442e9457caed9a9a
samuel.kebe <samuel.kebe@it-centrex.com>
2025-02-10 18:26:40 +0000

    commit

--- adapters/src/main/java/com/etix/adapters/config/DomainConfiguration.java
+++ adapters/src/main/java/com/etix/adapters/config/DomainConfiguration.java
@@ -1,13 +1,12 @@
 package com.etix.adapters.config;
 
-
-import com.itcentrex.domain.ddd.DomaineService;
+import com.etix.domain.ddd.DomaineService;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(basePackages = {"com.itcentrex.domain"},
+@ComponentScan(basePackages = {"com.etix.domain"},
         includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {DomaineService.class})}
 )
 public class DomainConfiguration {
